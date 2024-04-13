7a8a3225c82e2da9ec0350e952bcde21da63abd6
apoorva.ce <apoorva.ce@subex.com>
2024-04-14 00:23:43 +0530

    tagged template in JS

--- callingFunctionInVariousmethod.js
+++ callingFunctionInVariousmethod.js
@@ -31,3 +31,40 @@ const object = {
     }
 }
 object.show;
+
+
+"###############################################################################################################################################################"
+
+
+//Example 1 (Tagged template literal)
+
+
+
+const name ="Javascript";
+function showLiteral(a,b,c,d){
+    console.log("a=",a);//prints 'Hey' 'welcome' because first "a" accepts array of literal
+    console.log("b=",b);//prints Javascript
+    console.log("JA=",c);//undefined
+    console.log("JA=",d);//undefined
+
+}
+
+showLiteral`Hey ${name} , Welcome , to , Javacript`;
+
+//a receives ["Hey ", " , Welcome , ", ""] because it contains the string parts of the template literal.
+//b receives "Javascript" because it evaluates the expression ${name}.
+//c is undefined because there's no expression for it to evaluate in the template literal.
+
+//But we can also make c and d have values
+
+showLiteral`Hey ${name} , Welcome , ${name} , ${99}`;
+
+
+//In JavaScript, when you use template literals with tagged template strings, the template literal parts are passed to the tag function as an array of strings. 
+//Any expressions within ${} are passed as separate arguments following the array of strings.
+
+
+//So for "a"  array of strings are a arguement , and remaining expression literal(if any) are passed as an seperate arguement in eg1 only one expression literal was there so b was given that value
+//c and d were undefined , if we want assign c and d value we shlld pass expression literal
+
+"###############################################################################################################################################################"
